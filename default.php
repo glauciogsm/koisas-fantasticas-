4f2f3b6b1973ec896861e98fe7d545c59754f9c4
Glauciogsm <glauciogsm@hotmail.com>
2024-04-02 00:42:12 -0300

    base

---
+++ default.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de vendas</title>
+    <link rel="shortcut icon" href="abase/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="abase/style.css">    
+</head>
+<body>
+    <main>
+        <picture>
+            <source media="(max-width:600px)" srcset="abase/cabecalhooficial.png">
+            <img src="abase/cabecalhooficial.png" alt="Imagem principal.">
+        </picture>
+    </main>    
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Natação</a>
+        <a href="#">Utilidades</a>
+        <a href="#">Camping</a>
+        <a href="#">Praia</a>
+     </nav>
+     <div>
+        <table>            
+            <tr>                
+                    <td>Trompete</td>                
+                    <td>Trombone</td>   
+                    <td>Trompete</td>                
+                    <td>Trombone</td>                
+            </tr>
+            <tr>
+                    <td>Trompa</td>
+                    <td>Tuba</td>
+                    <td>Trompete</td>                
+                    <td>Trombone</td>   
+            </tr>
+                
+        </table>
+     </div>
+
+    <footer>
+        <p>Site criado pelo <a href="https://github.com/glauciogsm" target="_blank">glauciogsm versão 2.1.3</a></p>
+    </footer> 
+</body>
+</html>
